e1461cb68af154ef57b34ba5a8b6e6229a63d41e
mayuri <mayu.reddy10@gmail.com>
2026-02-04 18:52:53 +1100

    actions

--- common.sh
+++ common.sh
@@ -16,7 +16,7 @@ mkdir -p $logs_folder
 echo "$(date "+%y-%m-%d %H:%M:%S") | script started executing at: $(date)" | tee -a $logs_file
 
 check_root () {
-    if [ $userid -ne 0 ]; then
+    if [ $userid -ne 0 ]; then  $logs_file
        echo -e "$R Please run this script with root access $N" | tee -a $logs_file
        exit 1
     fi
@@ -90,6 +90,6 @@ validate $? "restarting $app_name"
 
 print_total_time() {
     end_time=$(date +%s)
-    total_time=$(( $end_time - $start_time ))
+    total_time=$(( end_time - start_time ))
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script executed in: $G $total_time seconds $N" | tee -a $logs_file
 }
