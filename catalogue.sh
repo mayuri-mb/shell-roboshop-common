e7487537bcf76facc4990a4a6085f19338229f70
mayuri <mayu.reddy10@gmail.com>
2026-02-04 18:09:41 +1100

    actions

---
+++ catalogue.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+#Loading data into mongodb
+cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>> $logs_file
+
+index=$(mongosh --host $mongodb_host --quiet -eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $index -le 0 ]; then
+   mongosh --host $mongodb_host </app/db/master-data.js
+   validate $? "Loading Products"
+else
+   echo -e "$(date "+%y-%m-%d %H:%M:%S") |Product already loaded..$Y Skipping $N" 
+fi
+
+app_restart
+print_total_time
+
--- common.sh
+++ common.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+script_dir=$PWD
+mongodb_host=mongodb.daws-88s.online
 
 start_time=$(date +%s)
 mkdir -p $logs_folder
@@ -27,6 +29,63 @@ validate() {
     else
      echo -e "$(date "+%y-%m-%d %H:%M:%S") | $2 $G success $N" | tee -a $logs_file
     fi
+}
+
+nodejs_setup () {
+dnf module disable nodejs -y &>> $logs_file
+validate $? "disabling nodejs default module"
+
+dnf module enable nodejs:20 -y &>> $logs_file
+validate $? "enabling nodejs 20"
+
+dnf install nodejs -y &>> $logs_file
+validate $? "installing nodejs"
+
+npm install &>> $logs_file
+validate $? "installing dependencies"
+}
+
+app_setup() {
+id roboshop
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $logs_file
+   validate $? "Creating system user"
+else
+   echo -e "Roboshop user already exist..$Y Skipping $N"
+fi  
+
+#Downloading the app
+    mkdir -p /app 
+    validate $? "creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>> $logs_file
+    validate $? "downloading $app_name code"
+
+    cd /app 
+    validate $? "moving to app directory"
+
+    rm -rf /app/*
+    validate $? "Removing existing code"
+
+    unzip /tmp/$app_name.zip &>> $logs_file
+    validate $? "Unzip $app_name code"
+}
+
+systemd_setup() {
+    cp $script_dir/$app_name.service /etc/systemd/system/$app_name.service  
+    validate $? "Created systemctl service"
+
+    systemctl daemon-reload
+    validate $? "Loading the service"
+
+    systemctl enable $app_name
+    systemctl start $app_name &>> $logs_file
+    validate $? "enabling and starting the service"
+}
+
+app_restart() {
+systemctl restart $app_name
+validate $? "restarting $app_name" 
 }
 
 print_total_time() {
