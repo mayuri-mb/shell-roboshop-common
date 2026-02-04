06675fc5e924de7877f8ecd52ead98c21848a178
mayuri <mayu.reddy10@gmail.com>
2026-02-04 12:02:08 +1100

    actions

---
+++ common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+userid=(id -u)
+logs_folder="logs/var/shell-roboshop-common"
+logs_file="$logs_folder/$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+start_time=$(date +%s)
+mkdir -p $logs_folder
+
+echo "$(date "+%y-%m-%d %h%m%s") | script started executing at: $(date)" | tee -a $logs_file
+
+check_root () {
+    if [ $userid -ne 0 ]; then
+       echo -e "$R Please run this script with root access $N" | tee -a $logs_file
+       exit 1
+    fi
+}
+
+validate() {
+    if [ $1 -ne 0 ]; then
+       echo -e "$(date "+%y-%m-%d %h%m%s") | $2 $R failed $N"  | tee -a $logs_file
+       exit 1
+    else
+       echo -e "$(date "+%y-%m-%d %h%m%s") | $2 $G success $N" | tee -a $logs_file"
+    fi
+}
+
+print_total_time() {
+    end_time=$(date +%s)
+    total_time=$( $end_time - $start_time )
+    echo -e "Script executed in: $G $total_time seconds $N" | tee -a $logs_file"
+}
---
+++ mongodb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+mkdir -p $logs_folder
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "copying mongo repo"
+
+dnf install mongodb-org -y &>>$logs_file
+validate $? "Installing mongodb server"
+
+systemctl enable mongod &>>$logs_file
+validate $? "Enable mongodb"
+
+systemctl start mongod &>>$logs_file
+validate $? "Start mongodb"
+ 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+validate $? "Allowing remote connections"
+
+systemctl restart mongod
+validate $? "Restarted mongodb"
