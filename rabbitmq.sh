89874ef89608e719d843148820ec1f80a6c1d8f1
mayuri <mayu.reddy10@gmail.com>
2026-02-04 22:21:16 +1100

    actions

---
+++ rabbitmq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=rabbitmq
+check_root
+
+cp $script_dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$logs_file
+validate $? "added rabitmq repo"
+
+dnf install rabbitmq-server -y &>>$logs_file
+validate $? "installing rabbitmq"
+
+systemctl enable rabbitmq-server &>>$logs_file
+systemctl start rabbitmq-server &>>$logs_file
+validate $? "enabled and started rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123 &>>$logs_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$logs_file
+validate $? "created users and given permissions" 
+
+ptint_total_time
